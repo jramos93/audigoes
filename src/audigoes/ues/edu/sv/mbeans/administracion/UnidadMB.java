0eb75d11c30c910a6c88d4a8a4911db3d3ebd9e7
Wilmer Grijalva <Wilmer Grijalva@DESKTOP-0QD5GVG>
2020-08-26 22:49:32 -0600

    catalogo de unidades

---
+++ src/audigoes/ues/edu/sv/mbeans/administracion/UnidadMB.java
@@ -0,0 +1,101 @@
+package audigoes.ues.edu.sv.mbeans.administracion;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import audigoes.ues.edu.sv.controller.AudigoesController;
+import audigoes.ues.edu.sv.entities.administracion.Unidad;
+
+@ManagedBean(name = "uniMB")
+@ViewScoped
+public class UnidadMB extends AudigoesController implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<Unidad> filteredUnidades;
+
+	public UnidadMB() {
+		super(new Unidad());
+	}
+
+	@PostConstruct
+	public void init() {
+		try {
+			super.init();
+			fillListado();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public void fillListado() {
+		try {
+			setListado((List<Unidad>) audigoesLocal.findByNamedQuery(Unidad.class, "unidad.all", new Object[] {}));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
+		String filterText = (filter == null) ? null : filter.toString().trim().toLowerCase();
+		if (filterText == null || filterText.equals("")) {
+			return true;
+		}
+		int filterInt = getInteger(filterText);
+
+		Unidad unidad = (Unidad) value;
+		return unidad.getUniNombre().toLowerCase().contains(filterText)
+				||unidad.getUniIniciales().toLowerCase().contains(filterText)
+				|| unidad.getUniId() == filterInt;
+	}
+
+	private int getInteger(String string) {
+		try {
+			return Integer.valueOf(string);
+		} catch (NumberFormatException ex) {
+			return 0;
+		}
+	}
+
+	/* GETS y SETS */
+
+	@Override
+	public Unidad getRegistro() {
+		return (Unidad) super.getRegistro();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Unidad> getListado() {
+		return (List<Unidad>) super.getListado();
+	}
+	
+	@Override
+	public boolean beforeSaveNew() {
+		getRegistro().setInstitucion(getObjAppsSession().getUsuario().getInstitucion());
+		return super.beforeSaveNew();
+	}
+
+	@Override
+	public void afterSaveNew() {
+		getListado().add(getRegistro());
+		super.afterSaveNew();
+	}
+
+	public List<Unidad> getFilteredUnidades() {
+		return filteredUnidades;
+	}
+
+	public void setFilteredUnidades(List<Unidad> filteredUnidades) {
+		this.filteredUnidades = filteredUnidades;
+	}
+}
