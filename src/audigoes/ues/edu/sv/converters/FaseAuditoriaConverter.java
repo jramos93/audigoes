0cb49c3dc4320d50b29cb59d9b5dfdf1518e172d
Wilmer Grijalva <wilmer.grijalva@gmail.com>
2020-11-16 19:05:33 -0600

    criterio.xhtml se agregó botón para pdf, corrección en nombre de faseAuditoriaConverter

--- src/audigoes/ues/edu/sv/converters/FaseAuditoriaConverter.java
+++ src/audigoes/ues/edu/sv/converters/FaseAuditoriaConverter.java
@@ -5,7 +5,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-@FacesConverter("regActivoConverter")
+@FacesConverter("faseAuditoriaConverter")
 public class FaseAuditoriaConverter implements Converter {
 
 	@Override
