86abfa2a42d8fc2f880589f15f6d210fb97eb3df
Wilmer Grijalva <wilmer.grijalva@gmail.com>
2020-12-12 12:30:42 -0600

    Consulta Auditorias

---
+++ src/audigoes/ues/edu/sv/entities/vistas/StatsAuditoriaEstado.java
@@ -0,0 +1,103 @@
+package audigoes.ues.edu.sv.entities.vistas;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigInteger;
+
+
+/**
+ * The persistent class for the vw_stats_auditorias_estados database table.
+ * 
+ */
+@Entity
+@SqlResultSetMapping(name = "StatsAuditoriaEstado", entities = {@EntityResult(entityClass = StatsAuditoriaEstado.class, fields = {
+		@FieldResult(name = "total", column = "TOTAL"), @FieldResult(name = "programada", column = "PROGRAMADA"),
+		@FieldResult(name = "planificacion", column = "PLANIFICACION"), @FieldResult(name = "ejecucion", column = "EJECUCION"),
+		@FieldResult(name = "informe", column = "INFORME"), @FieldResult(name = "seguimiento", column = "SEGUIMIENTO"),
+		@FieldResult(name = "finalizada", column = "FINALIZADA")
+})})
+public class StatsAuditoriaEstado implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="total")
+	private String total;
+
+	@Column(name="programada")
+	private BigInteger programada;
+	
+	@Column(name="planificacion")
+	private BigInteger planificacion;
+	
+	@Column(name="ejecucion")
+	private BigInteger ejecucion;
+	
+	@Column(name="informe")
+	private BigInteger informe;
+	
+	@Column(name="seguimiento")
+	private BigInteger seguimiento;
+	
+	@Column(name="finalizada")
+	private BigInteger finalizada;
+
+	public StatsAuditoriaEstado() {
+	}
+
+	public String getTotal() {
+		return this.total;
+	}
+
+	public void setTotal(String total) {
+		this.total = total;
+	}
+
+	public BigInteger getProgramada() {
+		return programada;
+	}
+
+	public void setProgramada(BigInteger programada) {
+		this.programada = programada;
+	}
+
+	public BigInteger getPlanificacion() {
+		return planificacion;
+	}
+
+	public void setPlanificacion(BigInteger planificacion) {
+		this.planificacion = planificacion;
+	}
+
+	public BigInteger getEjecucion() {
+		return ejecucion;
+	}
+
+	public void setEjecucion(BigInteger ejecucion) {
+		this.ejecucion = ejecucion;
+	}
+
+	public BigInteger getInforme() {
+		return informe;
+	}
+
+	public void setInforme(BigInteger informe) {
+		this.informe = informe;
+	}
+
+	public BigInteger getSeguimiento() {
+		return seguimiento;
+	}
+
+	public void setSeguimiento(BigInteger seguimiento) {
+		this.seguimiento = seguimiento;
+	}
+
+	public BigInteger getFinalizada() {
+		return finalizada;
+	}
+
+	public void setFinalizada(BigInteger finalizada) {
+		this.finalizada = finalizada;
+	}
+
+}
--- src/audigoes/ues/edu/sv/mbeans/consultas/ConsultaMB.java
+++ src/audigoes/ues/edu/sv/mbeans/consultas/ConsultaMB.java
@@ -1,6 +1,10 @@
 package audigoes.ues.edu.sv.mbeans.consultas;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
 
@@ -12,25 +16,35 @@ import audigoes.ues.edu.sv.controller.AudigoesController;
 import audigoes.ues.edu.sv.entities.administracion.Unidad;
 import audigoes.ues.edu.sv.entities.informe.CedulaNota;
 import audigoes.ues.edu.sv.entities.planeacion.Auditoria;
+import audigoes.ues.edu.sv.entities.vistas.StatsAuditoriaEstado;
 
-@ManagedBean(name = "uniMB")
+@ManagedBean(name = "consMB")
 @ViewScoped
 public class ConsultaMB extends AudigoesController implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private List<Unidad> filteredUnidades;
-	
+
 	private List<Auditoria> auditoriasList;
 	private List<Auditoria> filteredAuditorias;
 	private Auditoria auditoria;
-	
+
 	private List<CedulaNota> cedulaNotasList;
 	private List<CedulaNota> filteredCedulaNotas;
 	private CedulaNota cedulaNota;
 	
+	private List<StatsAuditoriaEstado> statsList;
+
+	private Date fechaInicio;
+	private Date fechaFin;
+	
+	private String strFechaInicio;
+	private String strFechaFin;
+
+	private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
 	public ConsultaMB() {
 		super(new Unidad());
@@ -39,8 +53,16 @@ public class ConsultaMB extends AudigoesController implements Serializable {
 	@PostConstruct
 	public void init() {
 		try {
+			Calendar calendar = Calendar.getInstance();
+			Date fecha = new Date();
+			calendar.set(GregorianCalendar.getInstance().get(Calendar.YEAR), Calendar.JANUARY, 1);
+			setFechaInicio(calendar.getTime());
+			calendar.set(GregorianCalendar.getInstance().get(Calendar.YEAR), Calendar.DECEMBER, 31);
+			setFechaFin(calendar.getTime());
+			
+			fillAuditoriasList();
+			//fillStatsAuditoriaEstado();
 			super.init();
-			fillListado();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -48,9 +70,37 @@ public class ConsultaMB extends AudigoesController implements Serializable {
 	}
 
 	@SuppressWarnings("unchecked")
-	public void fillListado() {
+	public void fillAuditoriasList() {
 		try {
-			setListado((List<Unidad>) audigoesLocal.findByNamedQuery(Unidad.class, "unidad.all", new Object[] {}));
+			//setStrFechaInicio(getFormatter().format(getFechaInicio()));
+			//setStrFechaFin(getFormatter().format(getFechaFin()));
+			setAuditoriasList(
+					(List<Auditoria>) audigoesLocal.findByNamedQuery(Auditoria.class, "consulta.auditorias.institucion",
+							new Object[] { getObjAppsSession().getUsuario().getInstitucion().getInsId(),
+									getFechaInicio(), getFechaFin() }));
+			System.out.println(getAuditoriasList().size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void fillStatsAuditoriaEstado() {
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void fillCedulaNotas() {
+		try {
+			//setStrFechaInicio(getFormatter().format(getFechaInicio()));
+			//setStrFechaFin(getFormatter().format(getFechaFin()));
+			setCedulaNotasList((List<CedulaNota>) audigoesLocal.findByNamedQuery(CedulaNota.class,
+					"consulta.cedulanota.institucion",
+					new Object[] { getObjAppsSession().getUsuario().getInstitucion().getInsId(),
+							getFechaInicio(), getFechaFin() }));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -65,8 +115,7 @@ public class ConsultaMB extends AudigoesController implements Serializable {
 
 		Unidad unidad = (Unidad) value;
 		return unidad.getUniNombre().toLowerCase().contains(filterText)
-				||unidad.getUniIniciales().toLowerCase().contains(filterText)
-				|| unidad.getUniId() == filterInt;
+				|| unidad.getUniIniciales().toLowerCase().contains(filterText) || unidad.getUniId() == filterInt;
 	}
 
 	private int getInteger(String string) {
@@ -89,7 +138,7 @@ public class ConsultaMB extends AudigoesController implements Serializable {
 	public List<Unidad> getListado() {
 		return (List<Unidad>) super.getListado();
 	}
-	
+
 	@Override
 	public boolean beforeSaveNew() {
 		getRegistro().setInstitucion(getObjAppsSession().getUsuario().getInstitucion());
@@ -108,5 +157,101 @@ public class ConsultaMB extends AudigoesController implements Serializable {
 
 	public void setFilteredUnidades(List<Unidad> filteredUnidades) {
 		this.filteredUnidades = filteredUnidades;
+	}
+
+	public List<Auditoria> getAuditoriasList() {
+		return auditoriasList;
+	}
+
+	public void setAuditoriasList(List<Auditoria> auditoriasList) {
+		this.auditoriasList = auditoriasList;
+	}
+
+	public List<Auditoria> getFilteredAuditorias() {
+		return filteredAuditorias;
+	}
+
+	public void setFilteredAuditorias(List<Auditoria> filteredAuditorias) {
+		this.filteredAuditorias = filteredAuditorias;
+	}
+
+	public Auditoria getAuditoria() {
+		return auditoria;
+	}
+
+	public void setAuditoria(Auditoria auditoria) {
+		this.auditoria = auditoria;
+	}
+
+	public List<CedulaNota> getCedulaNotasList() {
+		return cedulaNotasList;
+	}
+
+	public void setCedulaNotasList(List<CedulaNota> cedulaNotasList) {
+		this.cedulaNotasList = cedulaNotasList;
+	}
+
+	public List<CedulaNota> getFilteredCedulaNotas() {
+		return filteredCedulaNotas;
+	}
+
+	public void setFilteredCedulaNotas(List<CedulaNota> filteredCedulaNotas) {
+		this.filteredCedulaNotas = filteredCedulaNotas;
+	}
+
+	public CedulaNota getCedulaNota() {
+		return cedulaNota;
+	}
+
+	public void setCedulaNota(CedulaNota cedulaNota) {
+		this.cedulaNota = cedulaNota;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public SimpleDateFormat getFormatter() {
+		return formatter;
+	}
+
+	public void setFormatter(SimpleDateFormat formatter) {
+		this.formatter = formatter;
+	}
+
+	public String getStrFechaInicio() {
+		return strFechaInicio;
+	}
+
+	public void setStrFechaInicio(String strFechaInicio) {
+		this.strFechaInicio = strFechaInicio;
+	}
+
+	public String getStrFechaFin() {
+		return strFechaFin;
+	}
+
+	public void setStrFechaFin(String strFechaFin) {
+		this.strFechaFin = strFechaFin;
+	}
+
+	public List<StatsAuditoriaEstado> getStatsList() {
+		return statsList;
+	}
+
+	public void setStatsList(List<StatsAuditoriaEstado> statsList) {
+		this.statsList = statsList;
 	}
 }
